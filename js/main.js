fff7cc146783bfc3aed2f654cf35979e0912bb5a
AVISHEK DUTTA <109311355+logicbyavishek@users.noreply.github.com>
2026-01-23 02:57:17 +0530

    button click to page redirect
    Navigate to Canvas.html after click create button

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const createBtn = document.querySelector('.top-actions');
+createBtn.addEventListener('click', () => {
+  console.log('Create button clicked!');
+  window.location.href = "./canvas.html"; // Navigate to canvas.html
+});
